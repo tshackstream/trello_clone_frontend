0b28c39eed210d84872c2ff747e4ba355b829be7
sakaitakuya <sakaitakuya@team-lab.com>
2019-12-13 10:42:39 +0900

    初期設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s',
+    title: 'Trello Clone App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -36,6 +36,7 @@ export default {
   */
   buildModules: [
     '@nuxtjs/vuetify',
+    '@nuxt/typescript-build',
   ],
   /*
   ** Nuxt.js modules
