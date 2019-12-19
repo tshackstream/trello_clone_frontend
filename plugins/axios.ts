7dc35075846791760b20b36d8a6a044e0062909b
sakaitakuya <sakaitakuya@team-lab.com>
2019-12-19 12:19:07 +0900

    リファクタ

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,6 +1,7 @@
-import { Plugin } from '@nuxt/types';
+import { Plugin, Context } from '@nuxt/types';
 
-const axiosPlugin: Plugin = ({ $axios, store }): void => {
+const axiosPlugin: Plugin = (ctx: Context): void => {
+  const { $axios, store } = ctx;
   $axios.onError(error => {
     if (error.response == null) { return }
     store.dispatch('errorStatus/showError', '通信エラーが発生しました。');
